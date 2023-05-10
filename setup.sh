7281dcfb21d65f445e54a1c1261d10956e1676c6
Simone Selis <simone.selis@icloud.com>
2023-05-10 12:33:50 +0200

    install dockutil

--- setup.sh
+++ setup.sh
@@ -10,6 +10,9 @@ sudo scutil --set ComputerName "$hostname"
 # Get sudo permissions
 sudo -v
 
+# Create Desktop directory if it doesn't exist
+mkdir -p ~/Desktop
+
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
@@ -26,12 +29,15 @@ brew update
 # Install Homebrew cask
 brew install cask
 
+# Install dockutil
+brew install dockutil
+
 # Install packages from Brewfile
 brew bundle
 
 # Add apps to the dock
 for app in $(brew --prefix)/Caskroom/*/*.app; do
-    dockutil --add "$app" --no-restart
+    /usr/local/bin/dockutil --add "$app" --no-restart
 done
 
 # Restart the dock to apply changes
@@ -42,6 +48,3 @@ sudo systemsetup -setremotelogin on
 
 # Enable screen sharing
 sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.screensharing.plist
-
-# Add all the apps installed from Brewfile to the Dock
-defaults write com.apple.dock persistent-apps -array-add 
