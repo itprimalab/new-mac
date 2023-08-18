3c7a4a018ca00b97eb555dc805df6f7ceb3a0651
Simone Selis <simone.selis@icloud.com>
2023-08-18 11:42:48 +0200

    I think now it works

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Install Xcode Command Line Tools
+xcode-select --install
+
 # Set Mac hostname
 echo "Insert Mac name:"
 read hostname
@@ -10,11 +13,17 @@ sudo scutil --set ComputerName "$hostname"
 # Get sudo permissions
 sudo -v
 
+# Keep-alive: update existing `sudo` time stamp until `setup.sh` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Create Desktop directory if it doesn't exist
 mkdir -p ~/Desktop
 
 # Install Homebrew Silently
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Add Homebrew to PATH
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$(whoami)/.zprofile
 
 # Clone the repository into Desktop
 cd ~/Desktop
@@ -34,6 +43,10 @@ brew install dockutil
 
 # Install packages from Brewfile
 brew bundle
+
+# Delete all the apps from the dock
+/usr/local/bin/dockutil --remove all --no-restart
+killall Dock
 
 # Add apps to the dock
 for app in $(brew --prefix)/Caskroom/*/*.app; do
